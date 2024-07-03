71cce19949f52e659586365506d5090b0085613a
oreographer <oreogaminghk@gmail.com>
2024-07-03 20:35:11 +0530

    manage

---
+++ _data.js
@@ -0,0 +1,7 @@
+export const SITE_NAME = "Popcorn Planet";
+
+export const OWNER = {
+  name: "OreoGrapher",
+  portfolio: "https://oreographer.vercel.app/",
+  github: "https://github.com/oreographer",
+};
